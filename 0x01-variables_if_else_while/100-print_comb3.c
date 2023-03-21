5fe389c607bb8be4bd43baf00e64c2750d0bcab6
Yhaw Emma <owusuapraku1914@gmail.com>
2023-03-20 21:59:07 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints all possible combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			k = (i * 10) + j;
+
+			if (k != 89)
+			{
+				putchar((k / 10) + '0');
+				putchar((k % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				{
+					putchar((k / 10) + '0');
+					putchar((k % 10) + '0');
+				}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
