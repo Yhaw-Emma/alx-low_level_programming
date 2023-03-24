e52393d6cb6ca7ebcf7f2089cf96294cb1994691
Yhaw Emma <owusuapraku1914@gmail.com>
2023-03-24 05:26:27 +0000

    function that prints an integer

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long n = 612852475143;
+	long largest_factor = 0;
+	long i;
+
+	while (n % 2 == 0)
+	{
+		largest_factor = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+		largest_factor = i;
+		n /= i;
+		}
+	}
+	if (n > 2)
+	{
+		largest_factor = n;
+	}
+	printf("%ld\n", largest_factor);
+
+	return (0);
+}
+
+
