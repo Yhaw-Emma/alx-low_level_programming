6a9f35096f08614dc2838d19340a761f5b1694a9
Yhaw-Emma <120664601+Yhaw-Emma@users.noreply.github.com>
2023-07-02 05:13:11 +0000

    c singly list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,14 @@
  */
 void free_list(list_t *head)
 {
-    list_t *current = head;
+	list_t *current = head;
 
-    while (current != NULL)
-    {
-        list_t *next = current->next;
+	while (current != NULL)
+	{
+		list_t *next = current->next;
 
-        free(current->str);
-        free(current);
-        current = next;
-    }
+		free(current->str);
+		free(current);
+		current = next;
+	}
 }
