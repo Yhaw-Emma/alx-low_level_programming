82b5d8063b3a5289855f398975e4cd7e06c5f4e7
Yhaw-Emma <120664601+Yhaw-Emma@users.noreply.github.com>
2023-07-02 04:55:13 +0000

    c singly list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -16,5 +16,5 @@ size_t list_len(const list_t *h)
 		elementCount++;
 		h = h->next;
 	}
-	return elementCount;
+	return (elementCount);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,33 +12,32 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len = 0;
+	list_t *new_node;
+	unsigned int len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[len])
-    {
-        len++;
-    }
-    
+	while (str[len])
+	{
+		len++;
+	}
 
-    new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
 
-    return (*head);
+	return (*head);
 }
