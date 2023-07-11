ec8fcaf2b664c8a91a38da78a5a65c0294c28d8f
Yhaw Emma <owusuapraku1914@gmail.com>
2023-07-11 05:51:18 +0000

    c-file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,23 +10,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    ssize_t file_descriptor;
-    ssize_t bytes_written;
-    ssize_t bytes_read;
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_written;
+	ssize_t bytes_read;
  
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+		return (0);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
   
-    buffer = malloc(sizeof(char) * letters);
-        if (!buffer)
-            return (0);
-    bytes_read = read(file_descriptor, buffer, letters);
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	free(buffer);
+	close(file_descriptor);
   
-    free(buffer);
-    close(file_descriptor);
-  
-    return (bytes_written);
+	return (bytes_written);
 }
